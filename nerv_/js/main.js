2fbb095e3dadb7756f4139cf6c746a99df05faad
lee1419 <68256259+lee1419@users.noreply.github.com>
2020-07-31 14:04:19 +0900

    Add files via upload

---
+++ nerv_/js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('.bxslider').bxSlider({
+      captions:false,
+      auto:true,
+      autoControls:false,
+      stopAutoOnclick:true
+    });
+    /*
+$('.all_labo a').each(function(){
+  var img = $(this) .find('img');
+  var src_off = img .attr('src');
+  var src_on = src_off .replace('off','on'); 
+
+    $(this).hover(function(){img.attr('src',src_on)},
+                  function(){img.attr('src',src_off)}); //hover 끝
+  }); //.all_labo a
+*/
+
+});
+
+
